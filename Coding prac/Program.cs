ab069d7b4618d7c7baabc27553a22385c3fd5291
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-25 10:43:39 +0530

    [Arundhati] add demo

---
+++ Coding prac/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Coding_prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int p = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck==p)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+
+    }
+}
