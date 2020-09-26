6539b316d95e3f2cb12c8c1a574ddc7a0402c0a0
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-26 09:36:27 +0530

    [Arundhati] Use case 2 added

---
+++ Coding prac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Coding_prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 1;
+            int emp_rate = 20;
+            int emphrs;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == p)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            int empwage = emphrs * emp_rate;
+            Console.WriteLine(empwage);
+
+        }
+    }
+
+}
